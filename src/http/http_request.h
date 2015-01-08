fd89b280eba6caaa39b9b06855148a420c992062
sanpoo <sanpoos@gmail.com>
2015-01-08 13:07:26 +0800

    1.2.2
    *) log local variable to global for reducing stack occupy
     *) Makefile
    make clean add removing bin file
     *) Adjust src code structure
     *) add
    http request

---
+++ src/http/http_request.h
@@ -0,0 +1,22 @@
+#ifndef __HTTP_REQUEST_H__
+#define __HTTP_REQUEST_H__
+
+
+#define HTTP_UNKNOWN                   0x0001
+#define HTTP_GET                       0x0002
+#define HTTP_HEAD                      0x0004
+#define HTTP_POST                      0x0008
+#define HTTP_PUT                       0x0010
+#define HTTP_DELETE                    0x0020
+#define HTTP_MKCOL                     0x0040
+#define HTTP_COPY                      0x0080
+#define HTTP_MOVE                      0x0100
+#define HTTP_OPTIONS                   0x0200
+#define HTTP_PROPFIND                  0x0400
+#define HTTP_PROPPATCH                 0x0800
+#define HTTP_LOCK                      0x1000
+#define HTTP_UNLOCK                    0x2000
+#define HTTP_TRACE                     0x4000
+
+#endif
+
